b916d0e3c9ce90326b6f3e70d921746838b8bdb1
Gloria Kuliś <gloria.kulis@student.pk.edu.pl>
2023-06-06 15:17:42 +0200

    Changes to be committed: 	deleted:    pomidoro/src/main/java/ztpai/gloriakulis/pomidoro/db/controllers/SessionController.java 	deleted:    pomidoro/src/main/java/ztpai/gloriakulis/pomidoro/db/entity/Session.java 	deleted:    pomidoro/src/main/java/ztpai/gloriakulis/pomidoro/db/repositories/SessionRepository.java
    Changes not staged for commit:
    	modified:   pomidoro/frontend/public/css/achievements.css
    	modified:   pomidoro/frontend/public/css/footer.css
    	modified:   pomidoro/frontend/public/css/statistics.css
    	modified:   pomidoro/frontend/public/css/timer.css
    	modified:   pomidoro/frontend/public/css/todolist.css
    	modified:   pomidoro/frontend/src/actions/fetchData.js
    	modified:   pomidoro/frontend/src/components/achievements.js
    	modified:   pomidoro/frontend/src/components/extra/configuration.js
    	modified:   pomidoro/frontend/src/components/extra/timer_footer.js
    	modified:   pomidoro/frontend/src/components/login.js
    	modified:   pomidoro/frontend/src/components/statistics.js
    	modified:   pomidoro/frontend/src/components/timer.js
    	modified:   pomidoro/frontend/src/components/todolist.js
    	modified:   pomidoro/frontend/src/index.js
    	modified:   pomidoro/pom.xml
    	modified:   pomidoro/src/main/java/ztpai/gloriakulis/pomidoro/db/controllers/AchievementController.java
    	modified:   pomidoro/src/main/java/ztpai/gloriakulis/pomidoro/db/entity/User.java
    	modified:   pomidoro/src/main/resources/application.properties

--- pomidoro/src/main/java/ztpai/gloriakulis/pomidoro/db/repositories/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package ztpai.gloriakulis.pomidoro.db.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ztpai.gloriakulis.pomidoro.db.entity.Session;
-
-public interface SessionRepository extends JpaRepository<Session, Integer> {
-}
